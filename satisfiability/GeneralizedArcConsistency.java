5c1b5a173ed653ef4e8a4edd835e592529e48744
ralexy <44032920+ralexy@users.noreply.github.com>
2019-11-19 23:14:53 +0100

    Update GeneralizedArcConsistency.java

--- satisfiability/GeneralizedArcConsistency.java
+++ satisfiability/GeneralizedArcConsistency.java
@@ -1,3 +1,11 @@
+/**
+ *
+ * @author 21600639 : DENOUAL Axel
+ * @author 21910036 : ROUSSEAU Alexy
+ * @author 21907858 : SABATIER Brian
+ * 
+ */
+
 package satisfiability;
 
 import java.util.Deque;
