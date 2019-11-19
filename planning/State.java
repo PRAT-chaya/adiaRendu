a44ea76c871bffd6b5e31fb4289d4b2ab11a558d
ralexy <44032920+ralexy@users.noreply.github.com>
2019-11-20 00:33:45 +0100

    Update State.java

--- planning/State.java
+++ planning/State.java
@@ -33,11 +33,18 @@ public class State {
         return this.affectation;
     }
 
+    /**
+     * Vérifie si un Etat satisfait une précondition
+     * @param partialState
+     * @return 
+     */
     public boolean satisfies(Map<Variable, String> partialState) {
+        //Si l'affectation n'est pas contenue dans notre attribut Map<Variable, String> affectation on retourne vrai
         if (partialState.isEmpty()) {
             return true;
         }
         for (Variable x : partialState.keySet()) {
+            // Ou si la variable de notre Etat partiel récupéree n'est pas égale à celle de l'affectation on retourne faux
             if (!(affectation.containsKey(x)) || !(partialState.get(x).equals(affectation.get(x)))) {
                 return false;
             }
@@ -45,16 +52,25 @@ public class State {
         return true;
     }
 
+    /**
+     * Application d'une action
+     * @param action
+     * @return
+     * @throws IllegalArgumentException 
+     */
     public State apply(Action action) throws IllegalArgumentException {
+        // Si une action est applicable alors on peut avancer (disjonction)
         if (action.is_applicable(this)) {
             State newState = copy();
+            // Parcours des règles de l'action
             for (ActionRule rule : action.getRules()) {
+                // Si le nouvel etat satisfait les préconditions de la règle
                 if (newState.satisfies(rule.getPreconditions())) {
+                    // Alors on met à jour les effets du nouvel etat (State)
                     for (Variable var : rule.getEffets().keySet()) {
                         newState.affectation.replace(var, rule.getEffets().get(var));
                     }
                 }
-
             }
             return newState;
         } else {
@@ -62,6 +78,10 @@ public class State {
         }
     }
 
+    /**
+     * Méthode permettant de copier un etat
+     * @return 
+     */
     public State copy() {
         State stateCopy = new State();
         this.affectation.entrySet().forEach((entry) -> {
