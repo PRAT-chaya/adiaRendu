2e1fd48482050346dc8cc286e3e88f03be225495
ralexy <44032920+ralexy@users.noreply.github.com>
2019-11-19 23:17:52 +0100

    Update BinaryEqualityConstraint.java

--- representation/BinaryEqualityConstraint.java
+++ representation/BinaryEqualityConstraint.java
@@ -1,18 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *
+ * @author 21600639 : DENOUAL Axel
+ * @author 21910036 : ROUSSEAU Alexy
+ * @author 21907858 : SABATIER Brian
+ * 
  */
+
 package representation;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-/**
- *
- * @author ordinaute
- */
 public class BinaryEqualityConstraint extends BinaryConstraint {
 
     public BinaryEqualityConstraint(RestrictedDomain term1, RestrictedDomain term2) {
