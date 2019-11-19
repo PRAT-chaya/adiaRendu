18a63a6802176c9be3702be4e04ab64e27866063
ralexy <44032920+ralexy@users.noreply.github.com>
2019-11-19 23:12:10 +0100

    Update PlanningProblem.java

--- planning/PlanningProblem.java
+++ planning/PlanningProblem.java
@@ -1,8 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *
+ * @author 21600639 : DENOUAL Axel
+ * @author 21910036 : ROUSSEAU Alexy
+ * @author 21907858 : SABATIER Brian
+ * 
  */
+
 package planning;
 
 import java.util.Deque;
@@ -13,10 +16,6 @@ import java.util.Map;
 import java.util.Set;
 import representation.Variable;
 
-/**
- *
- * @author 21600639
- */
 public class PlanningProblem {
 
     private final State initialState;
