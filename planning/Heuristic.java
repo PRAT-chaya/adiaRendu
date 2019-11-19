6718af54e9424d856881868528f3b946eb5084ab
ralexy <44032920+ralexy@users.noreply.github.com>
2019-11-19 23:11:50 +0100

    Update Heuristic.java

--- planning/Heuristic.java
+++ planning/Heuristic.java
@@ -1,8 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *
+ * @author 21600639 : DENOUAL Axel
+ * @author 21910036 : ROUSSEAU Alexy
+ * @author 21907858 : SABATIER Brian
+ * 
  */
+
 package planning;
 
 public interface Heuristic {
