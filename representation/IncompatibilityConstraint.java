7e16e0556d8345d3f43f9641cc4b025a0d7c9c58
PRAT-chaya <55378168+PRAT-chaya@users.noreply.github.com>
2019-11-20 15:16:31 +0100

    Update IncompatibilityConstraint.java

--- representation/IncompatibilityConstraint.java
+++ representation/IncompatibilityConstraint.java
@@ -1,4 +1,12 @@
 package representation;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import satisfiability.GeneralizedArcConsistency;
 /**
  *
  * @author 21600639 : DENOUAL Axel
