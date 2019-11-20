de60ad8a5b52e944f71e21e58729298ed27e2b32
ralexy <44032920+ralexy@users.noreply.github.com>
2019-11-20 06:51:00 +0100

    Update PlanningProblem.java

--- planning/PlanningProblem.java
+++ planning/PlanningProblem.java
@@ -67,25 +67,38 @@ public class PlanningProblem {
         return PlanningProblem.dfs(this, state, plan, closed, maxR);
     }
 
+    /**
+     * Méthode permettant d'effectuer une recherche en profondeur
+     * @param problem
+     * @param state
+     * @param plan
+     * @param closed
+     * @param maxR
+     * @return 
+     */
     public static Deque<Action> dfs(PlanningProblem problem, State state,
             Deque<Action> plan, Set<State> closed, int maxR) {
+        // Si la taille du plan dépasse celle définie par la recherche maximale en profondeur
         if (plan.size() > maxR) {
             return null;
         }
+        // Si l'etat satisfait le but on le retourne
         if (state.satisfies(problem.getGoal())) {
             return plan;
         } else {
             for (Action action : problem.getAvailableActions()) {
                 if (action.is_applicable(state)) {
                     State next = state.apply(action);
-                    problem.increaseDfsProbe();
+                    problem.increaseDfsProbe(); // Incrémentation de l'attribut DfsProbe
+                    // Si les fermés ne contiennent pas notre etat
                     if (!closed.contains(next)) {
-                        plan.push(action);
-                        closed.add(next);
+                        plan.push(action); // alors on ajoute notre action au plan
+                        closed.add(next); // Et notre etat aux fermés
                         Deque<Action> subplan = dfs(problem, next, plan, closed, maxR);
+                        // Si le sous-plan est égal à null ou qu'il n'est pas vide on le retourne 
                         if (subplan != null && !subplan.isEmpty()) {
                             return subplan;
-                        } else {
+                        } else { // Sinon on pioche le premier
                             plan.pop();
                         }
                     }
@@ -99,6 +112,11 @@ public class PlanningProblem {
         return PlanningProblem.bfs(this);
     }
 
+    /**
+     * Recherche en largeur
+     * @param problem
+     * @return 
+     */
     public static Deque<Action> bfs(PlanningProblem problem) {
         Map<State, State> father = new HashMap();
         Map<State, Action> plan = new HashMap();
@@ -107,26 +125,26 @@ public class PlanningProblem {
         open.add(problem.getInitialState());
         father.put(problem.getInitialState(), null);
         while (!open.isEmpty()) {
-            State state = open.pollFirst();
+            State state = open.pollFirst(); // Récupération et suppression du 1er élément de la liste
             closed.add(state);
             for (Action action : problem.getAvailableActions()) {
                 if (action.is_applicable(state)) {
-                    problem.increaseBfsProbe();
+                    problem.increaseBfsProbe(); // Incrémentation de bfsProbe
                     State next = state.apply(action);
-                    if (!closed.contains(next) && !open.contains(next)) {
+                    if (!closed.contains(next) && !open.contains(next)) { // Si les ouverts & les fermés ne contiennent pas l'etat suivant
                         father.put(next, state);
                         plan.put(next, action);
-                        if (next.satisfies(problem.getGoal())) {
+                        if (next.satisfies(problem.getGoal())) { // Si il y a satisfaction on a trouvé une solution on la retourne
                             return getBFSPlan(father, plan, next);
                         } else {
-                            open.add(next);
+                            open.add(next); // Sinon on l'ajoute aux ouverts et on itère encore...
                         }
 
                     }
                 }
             }
         }
-        return null;
+        return null; // Si tous les ouverts ont été parcouru et qu'aucune solution n'a été trouvée on retourne null
     }
 
     protected static Deque<Action> getBFSPlan(Map<State, State> father, Map<State, Action> action, State goal) {
